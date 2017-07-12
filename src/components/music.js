c8bc6e98efac8ebc68b5f5e4b4ed6f02ebc50166
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-12 14:51:42 -0600

    added styling to text and grass on footer

--- src/components/music.js
+++ src/components/music.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import Image from './images/grass2.png'
 class MusicPlayer extends React.Component {
 
   render(){
     return(
-      <footer className="page-footer">
-      <iframe src={this.props.player} width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
-        <h3 className="white-text">Click a bug!</h3>
-      </footer>
+      <div>
+        <footer className="page-footer">
+        <img src={Image}></img>
+          <iframe src={this.props.player} width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+          <h3 className="white-text">Catch a bug for some music!</h3>
+        </footer>
+      </div>
     );
   }
 }
